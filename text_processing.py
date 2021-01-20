a35a97aa22e49103812d4a8080544ccac545ce83
PrimeOfMine <snrndi1125@gmail.com>
2021-01-20 16:37:00 +0900

    Update my code

--- text_processing.py
+++ text_processing.py
@@ -12,7 +12,6 @@ def normalize(input_string):
     """
      인풋으로 받는 스트링에서 정규화된 스트링을 반환함
      아래의 요건들을 충족시켜야함
-    * 모든 단어들은 소문자로 되어야함
     * 띄어쓰기는 한칸으로 되어야함
     * 앞뒤 필요없는 띄어쓰기는 제거해야함
 
@@ -33,7 +32,12 @@ def normalize(input_string):
              >>> tp.normalize(input_string2)
              'extra space'
     """
-    normalized_string = None
+    tmp = input_string
+    tmp = tmp.lower()
+    tmp = tmp.split()
+    tmp = " ".join(tmp)
+
+    normalized_string = tmp
     return normalized_string
 
 
@@ -56,7 +60,12 @@ def no_vowels(input_string):
             "Ths s n xmpl."
             >>> input_string2 = "We love Python!"
             >>> tp.normalize(input_string2)
-            ''W lv Pythn!'
+            'W lv Pythn!'
     """
-    no_vowel_string = None
+    tmp = input_string
+    del_char = 'aeiouAEIOU'
+    for ele in del_char:
+        tmp = tmp.replace(ele, '')
+
+    no_vowel_string = tmp
     return no_vowel_string
